81832f71f1f5a6c3a4e9df6db47fb915cb693f1f
Adam-rk <adam.rfk2@gmail.com>
2026-03-16 12:16:58 +0100

    fix: fixed path alias resolution

--- front/vite.config.ts
+++ front/vite.config.ts
@@ -3,9 +3,15 @@ import path from "node:path";
 import { reactRouter } from "@react-router/dev/vite";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
+import { fileURLToPath } from "node:url";
 
 
 export default defineConfig(() => ({
   plugins: [tailwindcss(), reactRouter()],
   base: '/maker-flow/', 
+  resolve: {
+    alias: {
+      '~': fileURLToPath(new URL('./', import.meta.url)),
+    },
+  },
 }));
