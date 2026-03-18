c67d5e0ac041c7c1589045e8ff83e61e1813bbda
Adam-rk <adam.rfk2@gmail.com>
2026-03-18 06:51:58 +0100

    fix: added twig only for dev

--- back/config/bundles.php
+++ back/config/bundles.php
@@ -2,6 +2,7 @@
 
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
+    Symfony\Bundle\TwigBundle\TwigBundle::class => ['dev' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
