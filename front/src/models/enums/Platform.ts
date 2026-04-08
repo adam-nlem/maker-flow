d79ca3cc9a3664bcd958c8963205ec55a0c70fbc
Adam-rk <adam.rfk2@gmail.com>
2026-04-08 12:10:44 +0200

    fix: added the app's logo as favicon

--- front/src/models/enums/Platform.ts
+++ front/src/models/enums/Platform.ts
@@ -1,11 +1,11 @@
 export enum Platform {
-    //Instagram = 'instagram',
+    Instagram = 'instagram',
     Youtube = 'youtube',
     //Tiktok = 'tiktok',
 }
 
 export const platformToFrenchTranslation: Record<Platform, string> = {
-    //[Platform.Instagram]: "Instagram",
+    [Platform.Instagram]: "Instagram",
     [Platform.Youtube]: "YouTube",
     //[Platform.Tiktok]: "TikTok",
 }
@@ -13,19 +13,19 @@ export const platformToFrenchTranslation: Record<Platform, string> = {
 export const platformOptions = Object.values(Platform)
 
 export const platformToBgClass: Record<Platform, string> = {
-    //[Platform.Instagram]: "bg-purple/30",
+    [Platform.Instagram]: "bg-purple/30",
     [Platform.Youtube]: "bg-red/30",
     //[Platform.Tiktok]: "bg-dark/30",
 }
 
 export const platformToTextClass: Record<Platform, string> = {
-    //[Platform.Instagram]: "text-purple",
+    [Platform.Instagram]: "text-purple",
     [Platform.Youtube]: "text-red",
     //[Platform.Tiktok]: "text-dark",
 }
 
 export const platformToIcon: Record<Platform, string> = {
-    //[Platform.Instagram]: "/icons/platforms/instagram.svg",
+    [Platform.Instagram]: "/icons/platforms/instagram.svg",
     [Platform.Youtube]: "/icons/platforms/youtube.svg",
     //[Platform.Tiktok]: "/icons/platforms/tiktok.svg",
 }
